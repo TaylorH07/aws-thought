c5a203ae035ff52e796d038c9b2c8d90975d0543
tayHolmes7 <taylorh2343@gmail.com>
2022-12-09 12:39:11 -0600

    created get and post routes

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -8,3 +8,81 @@ AWS.config.update(awsConfig);
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const table = "Thoughts";
 
+// get users
+router.get('/users', (req, res) => {
+    const params = {
+        TableName: table,
+    };
+    
+    //Scan return items in the table
+    dynamodb.scan(params, (err, data) => {
+        if (err) {
+            res.status(500).json(err); //an error occured
+        }
+
+        else {
+            res.json(data.Items);
+        }
+    });
+});
+
+// get thoughts from user
+router.get('/users/:username', (req, res) => {
+    console.log(`Querying for thought(s) from ${req.params.username}.`);
+
+    const params = {
+      TableName: table,
+      ProjectionExpression: "#th, #ca",
+      KeyConditionExpression: "#un = :user", // specifies search criteria
+      ExpressionAttributeNames: {
+        "#un": "username",
+        "#ca": "createdAt",
+        "#th": "thought",
+      },
+      ExpressionAttributeValues: {
+        ":user": req.params.username,
+      },
+      ProjectionExpression: "#th, #ca",
+      ScanIndexForward: false,
+    };
+
+    dynamodb.query(params, (err, data) => {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            res.status(500).json(err); // an error occured
+        }
+
+        else {
+            console.log("Query succeeded.");
+            res.json(data.Items)
+        }
+    });
+});
+
+// Create new user /api/users
+// Create new user
+router.post('/users', (req, res) => {
+    const params = {
+        TableName: table,
+        Item: {
+        username: req.body.username,
+        createdAt: Date.now(),
+        thought: req.body.thought,
+        },
+    };
+    dynamodb.put(params, (err, data) => {
+        if (err) {
+        console.error(
+            'Unable to add item. Error JSON:',
+            JSON.stringify(err, null, 2),
+        );
+        res.status(500).json(err); // an error occurred
+        }
+        else {
+            console.log('Added item:', JSON.stringify(data, null, 2));
+            res.json({ Added: JSON.stringify(data, null, 2) });
+        }
+    });
+});
+
+module.exports = router;
